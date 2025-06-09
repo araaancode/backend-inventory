fec3db65e696aa44076a7383eae0b0d5b4e11f86
araaancode <araaancode@gmail.com>
2025-06-09 16:27:20 +0330

    modify the User model

--- models/User.js
+++ models/User.js
@@ -1,109 +1,135 @@
 // models/User.js
 
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, 'First name is required'],
-    trim: true,
-    minlength: 2,
-    maxlength: 50,
-  },
-  lastName: {
-    type: String,
-    required: [true, 'Last name is required'],
-    trim: true,
-    minlength: 2,
-    maxlength: 50,
-  },
-  username: {
-    type: String,
-    trim: true,
-    minlength: [3, 'Username must be at least 3 characters'],
-    maxlength: [20, 'Username must be at most 20 characters'],
-    unique: true,
-    sparse: true 
-  },
-  nationalCode: {
-    type: String,
-    trim: true,
-    match: [/^\d{10}$/, 'National code must be exactly 10 digits'],
-    unique: true,
-    sparse: true 
-  },
-  gender: {
-    type: String,
-    enum: ['male', 'female', 'other'],
-  },
-  province: {
-    type: String,
-    trim: true,
-  },
-  city: {
-    type: String,
-    trim: true,
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email'],
-    unique: true,
-    sparse: true 
-  },
-  phone: {
-    type: String,
-    validate: {
-      validator: function (v) {
-        return /^09\d{9}$/.test(v);
-      },
-      message: props => `${props.value} is not a valid Iranian mobile number`,
+const userSchema = new mongoose.Schema(
+  {
+    // =============================================== required fields ===============================================
+    avatar: {
+      type: String,
+      default: "default.jpg",
     },
-    unique: true,
-    sparse: true 
-  },
-  avatar: {
-    type: String,
-    default: 'default.jpg',
-  },
-  role: {
-    type: String,
-    enum: ['admin', 'seller', 'customer'],
-    default: 'customer',
-    required: [true, 'User role is required']
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide a password'],
-    minlength: [8, 'Password must be at least 8 characters'],
-    select: false,
-  },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-  favoriteProducts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product',
-    }
-  ],
-}, { timestamps: true });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+    storeName: {
+      type: String,
+      required: [true, "Store Name required"],
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+
+    username: {
+      type: String,
+      trim: true,
+      minlength: [3, "Username must be at least 3 characters"],
+      maxlength: [20, "Username must be at most 20 characters"],
+      unique: true,
+      sparse: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 8,
+      select: false,
+    },
+
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        // This only works on CREATE and SAVE!!!
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords are not the same!",
+      },
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+
+    phone: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /^09\d{9}$/.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid Iranian mobile number`,
+      },
+      unique: true,
+      sparse: true,
+    },
+
+    // =============================================== end of required fileds ===============================================
+
+    firstName: {
+      type: String,
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+
+    nationalCode: {
+      type: String,
+      trim: true,
+      match: [/^\d{10}$/, "National code must be exactly 10 digits"],
+      unique: true,
+      sparse: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+    },
+    province: {
+      type: String,
+      trim: true,
+    },
+    city: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
+      unique: true,
+      sparse: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "seller", "customer"],
+      default: "customer",
+      required: [true, "User role is required"],
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
-userSchema.pre('save', function (next) {
-  if (!this.isModified('password') || this.isNew) return next();
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
@@ -119,17 +145,23 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
     return JWTTimestamp < changedTimestamp;
   }
   return false;
 };
 
 userSchema.methods.createPasswordResetToken = function () {
-  const resetToken = crypto.randomBytes(32).toString('hex');
-  this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
   return resetToken;
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
