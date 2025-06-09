7a5acad25ca9dae75be29695bb4941dad2d6208f
araaancode <araaancode@gmail.com>
2025-06-09 23:36:38 +0330

    add Catalog model added

--- models/Catalog.js
+++ models/Catalog.js
@@ -6,7 +6,19 @@ const validator = require("validator");
 
 const catalogSchema = new mongoose.Schema(
   {
-  
+    title:{
+      type: String,
+      required: [true, 'عنوان کاتالوگ الزامی است'],
+      trim: true,
+      minlength: [3, 'عنوان کاتالوگ باید حداقل ۳ کاراکتر باشد'],
+      maxlength: [100, 'عنوان کاتالوگ نمی‌تواند بیشتر از ۱۰۰ کاراکتر باشد'],
+      validate: {
+        validator: function(v) {
+          return /^[\u0600-\u06FFa-zA-Z0-9\s\-_،.]+$/.test(v);
+        },
+        message: 'عنوان کاتالوگ فقط می‌تواند شامل حروف، اعداد و علائم نگارشی باشد'
+      }
+    }
   },
   { timestamps: true }
 );
